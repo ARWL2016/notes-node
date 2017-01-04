493317a3a2c7b0071afbb32b4f03d444d3b568c2
Alistair Willis <alistairrwillis@gmail.com>
2017-01-04 13:43:08 +0900

    commit to stash

--- app.js
+++ app.js
@@ -5,15 +5,7 @@ const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
 
-// console.log(_.isString(true));
-// console.log(_.isString('And'));
-var filteredArray = _.uniq(["Ann", "Ann", 1, 2, 3, 4, 2, 3]);
-console.log(filteredArray);
 
-var res = notes.add(9, 10);
-console.log(`The result is ${res}`);
 
-var user = os.userInfo(); 
 
-fs.appendFile('greetings.txt', `Hello ${user.username}. You are ${notes.age}.\n`);
 
