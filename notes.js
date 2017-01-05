602a6f14240bcd9aa52415eb58108cbbdb439e19
Alistair Willis <alistairrwillis@gmail.com>
2017-01-05 16:37:23 +0900

    Note app completed. Yargs help functions added. Debugging and arrow-functions reviewed.

--- notes.js
+++ notes.js
@@ -1,46 +1,64 @@
-console.log('Starting notes.js');
 const fs = require('fs'); 
 
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json'); 
+        return JSON.parse(notesString);
+        
+    } catch (e) {
+        return []; 
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-    var notes = [];
+    var notes = fetchNotes();
     var note = {
         title, 
         body
     };
-    try {
-        var notesString = fs.readFileSync('notes-data.json'); 
-        console.log(notesString); 
-        notes = JSON.parse(notesString);
-        console.log(notes); 
-
-    } catch (e) {
-
-    }
-
-    var duplicateNotes = notes.filter((note) => {
-        return note.title === title; 
-    });
+    var duplicateNotes = notes.filter((note) => note.title === title);
     
-
-    notes.push(note); 
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes)); 
+    if (duplicateNotes.length === 0) {
+        console.log("note in duplicate statement = ", note);
+        notes.push(note); 
+        saveNotes(notes);
+        return note;  
+    }   
 };
 
 var getAll = () => {
-    console.log('Getting all notes');
+    return fetchNotes(); 
 }; 
 
 var getNote = (title) => {
-    console.log('Reading single note', title);
+    var notes = fetchNotes(); 
+    var readNote = notes.filter((note) => note.title === title);
+    return readNote[0]; 
 };
 
 var removeNote = (title) => {
-    console.log('Removing note', title);
+    var notes = fetchNotes(); 
+    var filteredNotes = notes.filter((note) => note.title !== title); 
+    saveNotes(filteredNotes); 
+
+    return notes.length !== filteredNotes; 
+};
+
+var logNote = (note) => {
+    debugger; 
+    console.log("------------------");
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
 };
 
 module.exports = {
     addNote, 
     getAll, 
     getNote, 
-    removeNote
+    removeNote, 
+    logNote
 };
---
+++ playground/debugging.js
@@ -0,0 +1,11 @@
+var person = {
+    name: 'Alistair'
+};
+
+person.age = 40; 
+
+debugger; 
+
+person.name = 'Mike';
+
+console.log(person);
