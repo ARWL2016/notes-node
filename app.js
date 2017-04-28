4a9685f768398bbdd7149c59806a70fa5c51ec7d
Alistair Willis <alistairrwillis@gmail.com>
2017-04-28 15:14:31 +0900

    update

--- app.js
+++ app.js
@@ -17,19 +17,19 @@ const bodyOptions = {
     };
 
 const argv = yargs
-.command('add', 'Add a new note', {
-    title: titleOptions, 
-    body: bodyOptions
-})
-.command('list', 'List all notes')
-.command('read', 'Read a note', {
-    title: titleOptions 
-})
-.command('remove', 'Remove a note', {
-    title: titleOptions
-})
-.help()
-.argv; 
+  .command('add', 'Add a new note', {
+      title: titleOptions, 
+      body: bodyOptions
+  })
+  .command('list', 'List all notes')
+  .command('read', 'Read a note', {
+      title: titleOptions 
+  })
+  .command('remove', 'Remove a note', {
+      title: titleOptions
+  })
+  .help()
+  .argv; 
 
 var command = argv._[0]; 
 
